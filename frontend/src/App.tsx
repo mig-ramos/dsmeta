192a0bdf6c3b6d1a87aa34697e52a481297a516b
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-07-13 12:11:23 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import NotificationButton from './componetns/NotificationButton'
 
 function App() {
 
 
   return (
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton />
+    </>
   )
 }
 
