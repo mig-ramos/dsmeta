a27a68e98124730b6cd633497352a730aaffb950
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-07-13 13:14:11 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
-import './App.css'
-import NotificationButton from './componetns/NotificationButton'
+import "./App.css";
+import Header from "./componetns/Header";
+import NotificationButton from "./componetns/NotificationButton";
+import SalesCard from "./componetns/SalesCard";
 
 function App() {
-
-
   return (
     <>
-    <h1>Olá mundo!</h1>
-    <NotificationButton />
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
